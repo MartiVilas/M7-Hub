0affed6ec3fb5e370c3430477f4b0a0ea7fccf88
Martí Vilàs <150129703+MartiVilas@users.noreply.github.com>
2025-03-21 12:21:39 +0000

    add css and  star part 5

--- A06/index.php
+++ A06/index.php
@@ -6,6 +6,8 @@
     <title>A06 - DataBases</title>
     <!-- Incluir Tailwind CSS desde el CDN -->
     <script src="https://cdn.tailwindcss.com"></script>
+    <!-- Icono de base de datos -->
+    <link rel="icon" type="image/png" href="https://cdn-icons-png.flaticon.com/512/4248/4248443.png">
 </head>
 <body class="bg-gray-100 text-gray-800 font-sans">
 
@@ -17,7 +19,7 @@
                 <li><a href="veureNoticiesCultura.php" class="text-gray-900 hover:text-blue-500 text-lg font-semibold transition duration-200">Veure noticies de Cultura</a></li>
                 <li><a href="veureNoticiesFebrer.php" class="text-gray-900 hover:text-blue-500 text-lg font-semibold transition duration-200">Veure noticies de Febrer</a></li>
                 <li><a href="numeroNoticiesFebrer.php" class="text-gray-900 hover:text-blue-500 text-lg font-semibold transition duration-200">Numero noticies febrer</a></li>
-                <li><a href="veureSeccions.php" class="text-gray-900 hover:text-blue-500 text-lg font-semibold transition duration-200">Veure seccions</a></li>
+                <li><a href="veureSeccions.php" class="text-gray-900 hover:text-blue-500 text-lg font-semibold transition duration-200">Veure noticies amb link</a></li>
             </ul>
         </div>
     </nav>
